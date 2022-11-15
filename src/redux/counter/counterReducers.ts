bb99c8b0fd303f3423d59627de95ff15b1403ed8
sinn <sinn@rikkeisoft.com>
2022-11-15 10:15:47 +0700

    add redux

---
+++ src/redux/counter/counterReducers.ts
@@ -0,0 +1,20 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+  value: number;
+}
+export default {
+  increment: (state: CounterState) => {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
+    state.value += 1;
+  },
+  decrement: (state: CounterState) => {
+    state.value -= 1;
+  },
+  incrementByAmount: (state: CounterState, action: PayloadAction<number>) => {
+    state.value += action.payload;
+  },
+};
